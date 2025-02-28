de85ac51ffecb490d0027f4979b1216e854a28b1
Daz <djones@yozu.co.uk>
2025-02-28 10:47:02 +0000

    remove whitespace from controller, add user uniqueness validation, added password confirmation field to registration form

--- lib/generators/authentication/templates/registrations_controller.rb
+++ lib/generators/authentication/templates/registrations_controller.rb
@@ -1,24 +1,24 @@
 class RegistrationsController < ApplicationController
   allow_unauthenticated_access
-   
-   def new
-     @user = User.new
-   end
- 
-   def create
-     @user = User.new(user_params)
-        
-     if @user.save
-       start_new_session_for @user
-       redirect_to after_authentication_url, notice: "You have successfully registered!"
-     else
-       render :new, status: :unprocessable_entity
-     end
-   end
- 
-   private
- 
-   def user_params
-     params.require(:user).permit(:email_address, :password)
-   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      start_new_session_for @user
+      redirect_to after_authentication_url, notice: "You have successfully registered!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email_address, :password)
+  end
  end
